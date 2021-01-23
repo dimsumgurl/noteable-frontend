d0cbbb3751c0f94c4042d2f3eaf14a4d6c5354c2
SeihakRithy Muth <Rithy58@gmail.com>
2021-01-23 18:18:46 -0500

    Added BrowserRouter to create SPA (#5)
    * Added react-router-dom
    
    * Implemented BrowserRouter
    
    * Added HomePage component
    
    * Changed '/' path to Home and '/app' path to App

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Route,
+} from 'react-router-dom';
+import Home from './components/HomePage/Home'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path='/app'>
+        <App />
+      </Route>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
